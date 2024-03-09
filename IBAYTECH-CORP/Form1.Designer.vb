02c914a6ac32a6bfb13ade0f1b1cbc33261b9e5c
Christian Q. Cleofas <cleofastiano27@gmail.com>
2024-03-09 14:12:10 +0800

    NO_TICKET - Create a database function

--- IBAYTECH-CORP/Form1.Designer.vb
+++ IBAYTECH-CORP/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,20 +20,92 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnInsert = New System.Windows.Forms.Button()
+        Me.txtValue1 = New System.Windows.Forms.TextBox()
+        Me.txtValue2 = New System.Windows.Forms.TextBox()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 369)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(1284, 292)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'btnInsert
+        '
+        Me.btnInsert.Location = New System.Drawing.Point(139, 310)
+        Me.btnInsert.Name = "btnInsert"
+        Me.btnInsert.Size = New System.Drawing.Size(75, 23)
+        Me.btnInsert.TabIndex = 1
+        Me.btnInsert.Text = "Insert"
+        Me.btnInsert.UseVisualStyleBackColor = True
+        '
+        'txtValue1
+        '
+        Me.txtValue1.Location = New System.Drawing.Point(359, 181)
+        Me.txtValue1.Name = "txtValue1"
+        Me.txtValue1.Size = New System.Drawing.Size(100, 20)
+        Me.txtValue1.TabIndex = 2
+        '
+        'txtValue2
+        '
+        Me.txtValue2.Location = New System.Drawing.Point(359, 207)
+        Me.txtValue2.Name = "txtValue2"
+        Me.txtValue2.Size = New System.Drawing.Size(100, 20)
+        Me.txtValue2.TabIndex = 3
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Location = New System.Drawing.Point(220, 310)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpdate.TabIndex = 4
+        Me.btnUpdate.Text = "Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(301, 310)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(75, 23)
+        Me.btnDelete.TabIndex = 5
+        Me.btnDelete.Text = "Delete"
+        Me.btnDelete.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1284, 661)
+        Me.Controls.Add(Me.btnDelete)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.txtValue2)
+        Me.Controls.Add(Me.txtValue1)
+        Me.Controls.Add(Me.btnInsert)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Consolidated Form"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnInsert As Button
+    Friend WithEvents txtValue1 As TextBox
+    Friend WithEvents txtValue2 As TextBox
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
 End Class
--- IBAYTECH-CORP/Form1.vb
+++ IBAYTECH-CORP/Form1.vb
@@ -1,3 +1,60 @@
-﻿Public Class Form1
+﻿Imports Npgsql
+Public Class Form1
+    Dim con As New NpgsqlConnection("Host=localhost;Port=5432;Username=postgres;Password=password;Database=db")
+    Dim command As NpgsqlCommand
+    Dim adapter As New NpgsqlDataAdapter
+    Dim dt As New DataTable
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        refreshData()
+    End Sub
+    Private Sub refreshData()
+        dt.Clear()
+        con.Open()
+        Dim query As String = "SELECT * FROM public.database"
+        adapter = New NpgsqlDataAdapter(query, con)
+        adapter.Fill(dt)
+        DataGridView1.DataSource = dt
+        con.Close()
+    End Sub
+    Private Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
+        con.Open()
+        Dim query As String = "INSERT INTO database (fname, lname) VALUES (@val1, @val2)"
+        command = New NpgsqlCommand(query, con)
+        command.Parameters.AddWithValue("@val1", txtValue1.Text)
+        command.Parameters.AddWithValue("@val2", txtValue2.Text)
+        command.ExecuteNonQuery()
+        con.Close()
+        refreshData()
+    End Sub
 
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        con.Open()
+        Dim query As String = "UPDATE database SET fname=@val1, lname=@val2 WHERE ID=@id"
+        command = New NpgsqlCommand(query, con)
+        command.Parameters.AddWithValue("@val1", txtValue1.Text)
+        command.Parameters.AddWithValue("@val2", txtValue2.Text)
+        command.Parameters.AddWithValue("@id", DataGridView1.CurrentRow.Cells(0).Value)
+        command.ExecuteNonQuery()
+        con.Close()
+        refreshData()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        con.Open()
+        Dim query As String = "DELETE FROM database WHERE ID=@id"
+        command = New NpgsqlCommand(query, con)
+        command.Parameters.AddWithValue("@id", DataGridView1.CurrentRow.Cells(0).Value)
+        command.ExecuteNonQuery()
+        con.Close()
+        refreshData()
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        If e.RowIndex >= 0 Then
+            Dim row As DataGridViewRow
+            row = Me.DataGridView1.Rows(e.RowIndex)
+            txtValue1.Text = row.Cells("Column1").Value.ToString
+            txtValue2.Text = row.Cells("Column2").Value.ToString
+        End If
+    End Sub
 End Class
